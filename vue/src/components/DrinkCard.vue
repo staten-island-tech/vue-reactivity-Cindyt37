906bdaa5ece48db196b0671ffbd9aff4a9888a34
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2023-04-02 18:02:22 -0400

    .

---
+++ vue/src/components/DrinkCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="choice">
+        <button class="drink" @click="$emit('addDrinkList')"> {{ drink }}. . . . ${{ price }}</button>
+        <img class="image" :src="drinkImage">
+        <h1 class="description">{{ description }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Card",
+    props: {
+        drink: String,
+        price: String,
+        drinkImage: String,
+        description: String,
+    }
+};
+</script>
+
+<style>
+.image {
+  height: 10rem;
+  object-fit: cover;
+  display: block;
+}
+.description {
+  font-size: 10px;
+  align-content: center;
+}
+.choice {
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+  width: 12.7vw;
+}
+</style>
